8c18dc01e98279c7259aab32f8dc73439e8e46c9
AlexFlorenco <aflorenco13@gmail.com>
2023-06-29 00:58:55 -0300

    refactor: Refatoração funções add, del e showAll retornam bool

--- Estoque.dart
+++ Estoque.dart
@@ -3,35 +3,28 @@ import 'Veiculo.dart';
 class Estoque {
   List<Veiculo> catalogo = [];
 
-  void listarEstoque() {
-    if (catalogo.isNotEmpty) {
-      for (var veiculo in catalogo) {
-        veiculo.exibirVeiculo();
-      }
-    } else {
-      print("Lista vazia");
-    }
-  }
-
-  void add(Veiculo) {
+  bool add(Veiculo) {
     catalogo.add(Veiculo);
-    print("Veículo adicionado com sucesso!");
+    return true;
   }
 
   bool del(String placa) {
     for (var veiculo in catalogo) {
       if (veiculo.placa == placa) {
-        catalogo.remove(catalogo.indexOf(veiculo));
+        catalogo.removeAt(catalogo.indexOf(veiculo));
         return true;
       }
     }
     return false;
   }
 
-  void showAll() {
-    for (var veiculo in catalogo) {
-      print(
-          "${veiculo.marca} ${veiculo.modelo} ${veiculo.anoFabricacao}\nPlaca: ${veiculo.placa}\nDisponível:${veiculo.estaDisponivel}\nDiária:${veiculo.valorDiaria}\n");
+  bool showAll() {
+    if (catalogo.isNotEmpty) {
+      for (var veiculo in catalogo) {
+        veiculo.exibirInformacoesVeiculo();
+      }
+      return true;
     }
+    return false;
   }
 }
